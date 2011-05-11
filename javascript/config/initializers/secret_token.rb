fb31061cddc679189fd267f81a08625574c9a80d
Naotaka Harada <harada@gaji.jp>
2011-05-11 20:03:25 +0900

    rails tickets を持ってきた

---
+++ javascript/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tickets::Application.config.secret_token = '9322beac30eb64843fab1e9641f1d2cb601343d078b71d9192033d7d8347248c7bdece6810dcaf309cb0a018d925b6e8a74a4cdf36ad9752dc01d0aa506df4cf'
